0576ad7ee83a5937d1a1c88163766fddc6b68a8f
Jongheon <joneheon3579@naver.com>
2020-12-12 14:11:07 +0900

    포스트 글 작성(Create), 조회(Read) 테스트 기능 구현

---
+++ src/test/java/com/jongheon/www/noticeboard/controller/PostControllerTest.java
@@ -0,0 +1,60 @@
+package com.jongheon.www.noticeboard.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jongheon.www.noticeboard.domain.dto.request.PostRequestDto;
+import com.jongheon.www.noticeboard.domain.repository.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PostControllerTest {
+
+    private static final String memberId = "memberId@naver.com";
+    private static final String title = "title";
+    private static final String content = "content";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    void create() throws Exception{
+        String ct = objectMapper.writeValueAsString(
+                PostRequestDto.builder().memberId(memberId).title(title).content(content).build());
+
+        // 포스팅 기능이 제대로 동작하는지 확인
+        mockMvc.perform(post("/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ct))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Post Success"));
+    }
+
+    @Test
+    void read() throws Exception{
+
+        MvcResult mvcResult = mockMvc.perform(get("/post?id=2"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+}
